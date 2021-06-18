103e65ec3c4c291637c816349fa586341a798d18
romafilyutitch <roma050399@yadnex.ru>
2021-06-18 19:06:43 +0300

    logs for connectionPool and dao layer added

--- src/main/java/by/epam/jwd/web/dao/AbstractDao.java
+++ src/main/java/by/epam/jwd/web/dao/AbstractDao.java
@@ -14,23 +14,28 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 public abstract class AbstractDao<T extends DbEntity> implements Dao<T> {
     private static final Logger logger = LogManager.getLogger(AbstractDao.class);
 
     private static final String GENERATED_KEY_COLUMN = "GENERATED_KEY";
-    private static final String SAVE_OPERATION_UNSUPPORTED_MESSAGE = "Save entity operation is unsupported for this service";
-    private static final String FIND_OPERATION_UNSUPPORTED_MESSAGE = "Find entities operation is unsupported for this service";
-    private static final String UPDATE_OPERATION_UNSUPPORTED_MESSAGE = "Update entity operation is unsupported for this service";
-    private static final String DELETE_OPERATION_UNSUPPORTED_MESSAGE = "Delete entity operation is unsupported for this service";
-    private static final String SAVED_ENTITY_WAS_NOT_FOUND_BY_ID_MESSAGE = "Entity was saved, but was not found by id %s";
-    private static final String COULD_NOT_SAVE_ENTITY_MESSAGES = "Could not save entity %s, Exception: %s";
-    private static final String COULD_NOT_FIND_ALL_ENTITIES_MESSAGE = "Could not find all entities, Exception : %s";
-    private static final String COULD_NOT_UPDATE_ENTITY_MESSAGE = "Could not update entity %s, Exception: %s";
-    private static final String COULD_NOT_DELETE_ENTITY_MESSAGE = "Could not delete entity with id %d, Exception : %s";
+    private static final String TRYING_TO_SAVE_ENTITY_MESSAGE = "Trying to save entity %s ";
+    private static final String SAVED_ENTITY_WAS_NOT_FOUND_BY_ID_MESSAGE = "Saved entity was not found by id %s";
+    private static final String ENTITY_WAS_SAVED_MESSAGE = "Entity %s was saved";
+    private static final String COULD_NOT_SAVE_ENTITY_MESSAGE = "Could not save entity %s ";
+    private static final String TRYING_TO_UPDATE_ENTITY_MESSAGE = "Trying to update entity %s";
+    private static final String ENTITY_WAS_UPDATED_MESSAGE = "Entity %s was updated";
+    private static final String COULD_NOT_UPDATE_ENTITY_MESSAGE = "Could not update entity %s ";
+    private static final String TRYING_TO_DELETE_ENTITY_MESSAGE = "Trying to delete entity with id %d";
+    private static final String ENTITY_WAS_DELETE_MESSAGE = "Entity with id %d was deleted";
+    private static final String COULD_NOT_DELETE_ENTITY_MESSAGE = "Could not delete entity with id %d";
+    private static final String TRYING_TO_FIND_ENTITIES_BY_SQL_MESSAGE = "Trying to find entities by sql %s";
+    private static final String ENTITIES_BY_SQL_WAS_FOUND_MESSAGE = "Entities by sql was found %s";
+    private static final String COULD_NOT_FIND_ENTITIES_BY_SQL_MESSAGE = "Could not find entities by sql %s";
+    private static final String TRYING_TO_FIND_ENTITIES_BY_PREPARED_SQL_MESSAGE = "Trying to find entities by prepared sql %s";
+    private static final String ENTITIES_BY_PREPARED_SQL_WAS_FOUND_MESSAGE = "Entities by prepared sql was found %s";
+    private static final String COULD_NOT_FIND_ENTITIES_BY_PREPARED_SQL_MESSAGE = "Entities by prepared sql was found %s";
 
     private final String findAllSql;
     private final String findByIdSql;
@@ -49,9 +54,7 @@ public abstract class AbstractDao<T extends DbEntity> implements Dao<T> {
 
     @Override
     public T save(T entity) {
-        if (saveSql == null) {
-            throw new UnsupportedOperationException(SAVE_OPERATION_UNSUPPORTED_MESSAGE);
-        }
+        logger.info(String.format(TRYING_TO_SAVE_ENTITY_MESSAGE, entity));
         try (Connection connection = ConnectionPool.getConnectionPool().takeFreeConnection();
              PreparedStatement saveStatement = connection.prepareStatement(saveSql, Statement.RETURN_GENERATED_KEYS)) {
             setSavePrepareStatementValues(entity, saveStatement);
@@ -60,93 +63,92 @@ public abstract class AbstractDao<T extends DbEntity> implements Dao<T> {
             generatedKeyResultSet.next();
             Long id = generatedKeyResultSet.getLong(GENERATED_KEY_COLUMN);
             final Optional<T> optionalEntity = findById(id);
-            if(!optionalEntity.isPresent()) {
+            if (!optionalEntity.isPresent()) {
                 logger.error(String.format(SAVED_ENTITY_WAS_NOT_FOUND_BY_ID_MESSAGE, entity));
                 throw new DAOException(String.format(SAVED_ENTITY_WAS_NOT_FOUND_BY_ID_MESSAGE, entity));
             }
+            logger.info(String.format(ENTITY_WAS_SAVED_MESSAGE, entity));
             return optionalEntity.get();
         } catch (SQLException e) {
-            logger.error(String.format(COULD_NOT_SAVE_ENTITY_MESSAGES, entity, e));
-            throw new DAOException(e);
+            logger.error(String.format(COULD_NOT_SAVE_ENTITY_MESSAGE, entity), e);
+            throw new DAOException(String.format(COULD_NOT_SAVE_ENTITY_MESSAGE, entity), e);
         }
     }
 
     @Override
-    public List<T> findAll(){
-        if (findAllSql == null) {
-            throw new UnsupportedOperationException(FIND_OPERATION_UNSUPPORTED_MESSAGE);
-        }
+    public List<T> findAll() {
         return findEntities(findAllSql);
     }
 
     @Override
     public Optional<T> findById(Long id) {
-        if (findByIdSql == null) {
-            throw new UnsupportedOperationException("Find by id operation is unsupported");
-        }
         final List<T> foundEntities = findPreparedEntities(findByIdSql, statement -> statement.setLong(1, id));
         return foundEntities.stream().findAny();
     }
 
     @Override
-    public T update(T entity){
-        if (updateSql == null) {
-            throw new UnsupportedOperationException(UPDATE_OPERATION_UNSUPPORTED_MESSAGE);
-        }
+    public T update(T entity) {
+        logger.info(String.format(TRYING_TO_UPDATE_ENTITY_MESSAGE, entity));
         try (Connection connection = ConnectionPool.getConnectionPool().takeFreeConnection();
              PreparedStatement updateStatement = connection.prepareStatement(updateSql)) {
             setUpdatePreparedStatementValues(entity, updateStatement);
             updateStatement.executeUpdate();
+            logger.info(String.format(ENTITY_WAS_UPDATED_MESSAGE, entity));
             return entity;
         } catch (SQLException e) {
-            logger.error(String.format(COULD_NOT_UPDATE_ENTITY_MESSAGE, entity, e));
-            throw new DAOException(e);
+            logger.error(String.format(COULD_NOT_UPDATE_ENTITY_MESSAGE, entity), e);
+            throw new DAOException(String.format(COULD_NOT_UPDATE_ENTITY_MESSAGE, entity), e);
         }
     }
 
     @Override
-    public void delete(Long id){
-        if (deleteSql == null) {
-            throw new UnsupportedOperationException(DELETE_OPERATION_UNSUPPORTED_MESSAGE);
-        }
+    public void delete(Long id) {
+        logger.info(String.format(TRYING_TO_DELETE_ENTITY_MESSAGE, id));
         try (Connection connection = ConnectionPool.getConnectionPool().takeFreeConnection();
              PreparedStatement deleteStatement = connection.prepareStatement(deleteSql)) {
             deleteStatement.setLong(1, id);
             deleteStatement.executeUpdate();
+            logger.info(String.format(ENTITY_WAS_DELETE_MESSAGE, id));
         } catch (SQLException e) {
-            logger.error(String.format(COULD_NOT_DELETE_ENTITY_MESSAGE, id, e));
-            throw new DAOException(e);
+            logger.error(String.format(COULD_NOT_DELETE_ENTITY_MESSAGE, id), e);
+            throw new DAOException(String.format(COULD_NOT_DELETE_ENTITY_MESSAGE, id), e);
         }
     }
 
     protected List<T> findEntities(String sql) {
         final List<T> foundEntities = new ArrayList<>();
+        logger.info(String.format(TRYING_TO_FIND_ENTITIES_BY_SQL_MESSAGE, sql));
         try (Connection connection = ConnectionPool.getConnectionPool().takeFreeConnection();
-        Statement findAllStatement = connection.createStatement();
-        ResultSet resultSet = findAllStatement.executeQuery(sql)) {
+             Statement findAllStatement = connection.createStatement();
+             ResultSet resultSet = findAllStatement.executeQuery(sql)) {
             while (resultSet.next()) {
                 final T foundEntity = mapResultSet(resultSet);
                 foundEntities.add(foundEntity);
             }
+            logger.info(String.format(ENTITIES_BY_SQL_WAS_FOUND_MESSAGE, sql));
             return foundEntities;
         } catch (SQLException e) {
-            throw new DAOException(e);
+            logger.error(String.format(COULD_NOT_FIND_ENTITIES_BY_SQL_MESSAGE, sql), e);
+            throw new DAOException(String.format(COULD_NOT_FIND_ENTITIES_BY_SQL_MESSAGE, sql), e);
         }
     }
 
-    protected List<T> findPreparedEntities(String sql, SQLConsumer<PreparedStatement> preparedStatementConsumer) {
+    protected List<T> findPreparedEntities(String preparedSql, SQLConsumer<PreparedStatement> preparedStatementConsumer) {
         final List<T> foundEntities = new ArrayList<>();
+        logger.info(String.format(TRYING_TO_FIND_ENTITIES_BY_PREPARED_SQL_MESSAGE, preparedSql));
         try (Connection connection = ConnectionPool.getConnectionPool().takeFreeConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(preparedSql)) {
             preparedStatementConsumer.accept(preparedStatement);
             final ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 final T foundEntity = mapResultSet(resultSet);
                 foundEntities.add(foundEntity);
             }
+            logger.info(String.format(ENTITIES_BY_PREPARED_SQL_WAS_FOUND_MESSAGE, preparedSql));
             return foundEntities;
         } catch (SQLException e) {
-            throw new DAOException(e);
+            logger.error(String.format(COULD_NOT_FIND_ENTITIES_BY_PREPARED_SQL_MESSAGE, preparedSql), e);
+            throw new DAOException(String.format(COULD_NOT_FIND_ENTITIES_BY_PREPARED_SQL_MESSAGE, preparedSql), e);
         }
     }
 
