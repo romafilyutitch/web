135642a647e06f5db6f46c491227bf03bf7a7bc5
romafilyutitch <roma050399@yadnex.ru>
2021-08-22 18:58:51 +0300

    LikeDaoTest bug was fixed

--- src/test/java/by/epam/jwd/web/dao/MySQLLikeDaoTest.java
+++ src/test/java/by/epam/jwd/web/dao/MySQLLikeDaoTest.java
@@ -90,6 +90,7 @@ public class MySQLLikeDaoTest {
         final Book savedBook = MySQLBookDao.getInstance().save(newBook);
         testLike = new Like(testLike.getId(), testLike.getUser(), savedBook);
         final Like updatedLike = testDao.update(testLike);
+        MySQLBookDao.getInstance().delete(savedBook.getId());
         assertNotNull(updatedLike);
         assertEquals(savedBook, updatedLike.getBook());
         assertEquals(testLike, updatedLike);
